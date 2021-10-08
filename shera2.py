092e50ab727635ea3e44033f7b885adbad326242
Maiadown <92146909+Maiadown@users.noreply.github.com>
2021-10-08 13:03:30 +0600

    Add files via upload

---
+++ shera2.py
@@ -0,0 +1,25 @@
+Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:43:54) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python38-32/shera2.py =
+Введите первое число5
+Введите второе число7
+Result: 32
+Result: 18
+Result: 3.5714285714285716
+Result: 175
+Result: 6103515625
+Result: 3
+hihi
+>>> 
+== RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python38-32/shera2.py =
+Введите первое число5
+Введите второе число7
+Result: 32
+Result: 18
+Result: 3.5714285714285716
+Result: 175
+Result: 6103515625
+Result: 3
+HiHi
+>>> 
